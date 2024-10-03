8eb92079f371cfe576e4cb44a5f27c7bda20e3b4
Yondo90 <yondo@gmail.com>
2024-10-03 13:44:12 +0300

    finish Backend

--- Backend/controllers/task.controller.ts
+++ Backend/controllers/task.controller.ts
@@ -1,6 +1,29 @@
-import {PrismaClient} from "prisma";
+import { PrismaClient } from "@prisma/client";
+import { RequestHandler } from "express";
 
-import {RequestHandler} from "express"
+const prisma = new PrismaClient();
+export const createTaskJobs: RequestHandler = async (req,res,next) => {
+    const createTask = await prisma.taskJobs.create({
+        data:{...req.body},
+    });
+    res.status(200).json(createTask);
+};
+
+export const getTaskJobs:RequestHandler = async (req,res,next) =>{
+    const getAll = await prisma.taskJobs.findMany({});
+    res.status(200).json(getAll);
+};
 
 
-export const createTask: RequestHandler = async() =>{}
+
+export const getTaskJobsByName:RequestHandler = async (req,res,next) =>{
+    const {title} = req.body.title
+    const getByName = await prisma.taskJobs.findUnique({
+        where:{
+            title,
+        },
+    });
+    res.status(200).json(getByName);
+};
+
+
